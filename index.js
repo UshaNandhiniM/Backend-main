c1d594675a90c92177c99b17c42b39bcb8271967
UshaNandhiniM <ushanandhinim123@gmail.com>
2024-08-01 07:18:55 +0530

    updated

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv'
-import connectDB from './Database/Config.js';
-import userRouter from './Routers/userRoute.js';
-import productRoute from './Routers/productRoute.js'
-import orderRoute from './Routers/orderRoute.js'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./Database/Config.js";
+import userRouter from "./Routers/userRoute.js";
+import productRoute from "./Routers/productRoute.js";
+import orderRoute from "./Routers/orderRoute.js";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
-app.use(cors({
-    origin:"*",
+app.use(
+  cors({
+    origin: "*",
     credentials: true,
-}));
+  })
+);
 
 //DB connection
 connectDB();
@@ -29,17 +31,15 @@ app.use((err, req, res, next) => {
   });
 });
 
-
 //route
-app.use('/api/user',userRouter)
-app.use('/api/product',productRoute)
-app.use('/api/order',orderRoute)
+app.use("/api/user", userRouter);
+app.use("/api/product", productRoute);
+app.use("/api/order", orderRoute);
 
-app.get('/', (req, res) => {
-  res.status(200).send("hi welcome car service project")
+app.get("/", (req, res) => {
+  res.status(200).send("hi welcome car service project");
+});
 
-})
-
-app.listen(process.env.PORT, () =>{
- console.log('Server running on port')
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port");
 });
