322b5399ed3db149ecf87383aa027ede09bdf953
UshaNandhiniM <ushanandhinim123@gmail.com>
2024-08-04 15:34:41 +0530

    updated

--- Contollers/productController.js
+++ Contollers/productController.js
@@ -1,7 +1,7 @@
 import Product from "../Models/productSchema.js";
 import { errorHandler } from "../utils/errorHandler.js";
 
-export const createProduct = async (req, res,next) => {
+export const createProduct = async (req, res, next) => {
   const { name, price, description, ratings, images } = req.body;
   const newproduct = new Product({ name, price, description, ratings, images });
   try {
@@ -10,7 +10,7 @@ export const createProduct = async (req, res,next) => {
       .status(200)
       .json({ message: "Product created successfully", result: newproduct });
   } catch (err) {
-   next({ message: "Internal error creating a product" });
+    next({ message: "Internal error creating a product" });
   }
 };
 
@@ -27,7 +27,7 @@ export const getProductById = async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
     if (!product) {
-      return next(errorHandler(404, "Product not found" ));
+      return next(errorHandler(404, "Product not found"));
     }
     res.status(200).json({ message: "Product details", result: product });
   } catch (err) {
@@ -38,37 +38,35 @@ export const getProductById = async (req, res) => {
 export const updateProduct = async (req, res) => {
   try {
     const id = req.params.id;
-    const updatedProduct=await Product.findByIdAndUpdate({_id:id},{
-      name:req.body.name,
-      price:req.body.price,
-      description:req.body.description,
-      ratings:req.body.ratings,
-      images:req.body.images,
-
-    })
+    const updatedProduct = await Product.findByIdAndUpdate(
+      { _id: id },
+      {
+        name: req.body.name,
+        price: req.body.price,
+        description: req.body.description,
+        ratings: req.body.ratings,
+        images: req.body.images,
+      }
+    );
     if (!updatedProduct) {
-      return next(errorHandler(404, "Product not found" ));
+      return next(errorHandler(404, "Product not found"));
     }
-    res
-      .status(200)
-      .json({
-        message: "Product updated successfully",result:updatedProduct
-      });
+    res.status(200).json({
+      message: "Product updated successfully",
+      result: updatedProduct,
+    });
   } catch (err) {
     res.status(500).json({ message: "Internal error updating a product" });
   }
 };
 
 export const deleteProduct = async (req, res) => {
-  
   try {
     const id = req.params.id;
-    const deletedProduct = await Product.findByIdAndDelete({_id:id});
-    res
-      .status(200)
-      .json({
-        message: "Product deleted successfully"
-      });
+    const deletedProduct = await Product.findByIdAndDelete({ _id: id });
+    res.status(200).json({
+      message: "Product deleted successfully",
+    });
   } catch (err) {
     res.status(500).json({ message: "Internal error deleting a product" });
   }
